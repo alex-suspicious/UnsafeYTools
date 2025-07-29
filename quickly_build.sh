d246f05342f15e00947ab32c99805ef634025cd7
alex-suspicious <savav.cool@gmail.com>
2025-07-29 23:14:15 +0500

    Latest changes

--- quickly_build.sh
+++ quickly_build.sh
@@ -1,8 +1,12 @@
 cd cpp
-g++ main.cpp -o ../video_processor -fpermissive \
-    `pkg-config --static opencv4 --cflags --libs` \
-    -lglfw -lGLEW -lGL -lpthread -ldl
-cp ../video_processor ../electron/video_processor
+
+#g++ main.cpp -o ../video_processor -fpermissive \
+#    -I/media/alex/Big\ Chungus1/Development/Sources/Chrome/new/opencv/modules/videoio/include \
+#    `pkg-config --cflags --libs --static opencv4` \
+#    -Wl,-Bstatic -lopencv_videoio \
+#    -Wl,-Bdynamic -lglfw -lGLEW -lGL -lpthread -ldl
+
+#cp ../video_processor ../electron/video_processor
 #cp ../video_processor.exe ../electron/video_processor.exe
-cd ../electron
-npm run build
+#cd ../electron
+#npm run build
